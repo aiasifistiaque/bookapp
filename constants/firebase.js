6891c8deaa9cacfe729153a753ba3fad0647b109
aiasifistiaque <43393686+aiasifistiaque@users.noreply.github.com>
2021-03-24 22:25:36 +0600

    Update firebase.js

--- constants/firebase.js
+++ constants/firebase.js
@@ -1,11 +1,5 @@
 //config file for firebase
 
 export const firebaseConfig = {
-	apiKey: 'AIzaSyCUm7wg0hrx1lYZq3OKVdtRo_J4qxE47Mo',
-	authDomain: 'port-book.firebaseapp.com',
-	projectId: 'port-book',
-	storageBucket: 'port-book.appspot.com',
-	messagingSenderId: '708739620581',
-	appId: '1:708739620581:web:7861c50dbe5ef32284be0c',
-	measurementId: 'G-H4DJZHJQ14',
+	
 };
